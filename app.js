2f7bc2fbe4703543a25eec45ff646d7f4ff93ce6
Mikael Ek <mikael.ek@cygni.se>
2014-08-30 14:42:15 +0200

    bower.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/assets',  express.static(__dirname + '/assets'));
 
 app.listen(3000, function () {
   console.log('Server running.');
