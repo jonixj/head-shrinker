c925a3ae4821c3406030aedc7beaf0685a2fe12e
Mikael Ek <mikael.ek@cygni.se>
2014-08-30 13:10:15 +0200

    Web component, gogo

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.listen(3000, function () {
   console.log('Server running.');
